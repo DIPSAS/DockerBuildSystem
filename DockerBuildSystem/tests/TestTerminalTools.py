b0bbbd9218f6e3a120bf424cd18ca87a9754426e
Hans Erik Heggem <hans.erik.heggem@gmail.com>
2019-02-25 00:31:56 +0100

    added more terminal tools tests

--- DockerBuildSystem/tests/TestTerminalTools.py
+++ DockerBuildSystem/tests/TestTerminalTools.py
@@ -1,5 +1,6 @@
 import unittest
 import os
+from . import TestTools
 from .. import TerminalTools
 
 class TestTerminalTools(unittest.TestCase):
@@ -8,6 +9,27 @@ class TestTerminalTools(unittest.TestCase):
         numbers = TerminalTools.GetNumbersFromString('per 123 k')
         self.assertEqual(len(numbers), 1)
         self.assertEqual(numbers[0], 123)
+
+
+    def test_ExecuteTerminalCommands(self):
+        cwd = TestTools.ChangeToSampleFolderAndGetCwd()
+        cmd = 'python ./pythonSnippet.py'
+        TerminalTools.ExecuteTerminalCommands([cmd], True)
+        os.chdir(cwd)
+
+
+    def test_ExecuteTerminalCommandAndGetOutput(self):
+        cmd = 'docker --version'
+        version = TerminalTools.ExecuteTerminalCommandAndGetOutput(cmd)
+        print(version)
+        self.assertTrue('version' in str(version).lower())
+
+
+    def test_ExportVariableToEnvironment(self):
+        variable = 'variable'
+        variableName = 'variableName'
+        TerminalTools.ExportVariableToEnvironment(variable, variableName)
+        self.assertTrue(variableName in os.environ)
 
 
 if __name__ == '__main__':
