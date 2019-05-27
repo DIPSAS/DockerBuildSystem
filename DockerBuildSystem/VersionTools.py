a6657c93ed8a418ef4ee6f13648324c9e6df2654
Hans Erik Heggem <heh@dips.no>
2019-05-27 15:14:27 +0200

    updated changelog features

--- DockerBuildSystem/VersionTools.py
+++ DockerBuildSystem/VersionTools.py
@@ -9,12 +9,24 @@ def GetVersionFromChangelog(changelogFile):
         lines = f.readlines()
     for line in lines:
         splitLine = line.split()
-        if len(splitLine) < 4:
+        if len(splitLine) == 0:
             continue
-        if splitLine[0] == "###" and splitLine[1].upper() == "VERSION":
+
+        # General criterias
+        if not(splitLine[0] == "##" or splitLine[0] == "###"):
+            continue
+
+        # Changelog version 1
+        if len(splitLine) >= 3 and splitLine[1].upper() == "VERSION":
             version = splitLine[2].replace(",", "")
             return version
+
+        # Changelog version 2
+        elif len(splitLine) >= 2 and splitLine[1][0] == '[' and splitLine[1][-1] == ']':
+            version = splitLine[1][1:-1].replace(",", "").replace(" ", "")
+            return version
+
     raise Exception("No version found in changelog file: " + str(changelogFile))
-
-
+        
+    
 
--- DockerBuildSystem/tests/TestVersionTools.py
+++ DockerBuildSystem/tests/TestVersionTools.py
@@ -7,14 +7,25 @@ class TestVersionTools(unittest.TestCase):
 
     def test_GetVersionFromChangelog(self):
         cwd = TestTools.ChangeToSampleFolderAndGetCwd()
+        
         version = VersionTools.GetVersionFromChangelog('CHANGELOG.md')
         self.assertEqual(version, '1.0.0')
+        version = VersionTools.GetVersionFromChangelog('CHANGELOG.v2.md')
+        self.assertEqual(version, '1.0.0')
+        
         os.chdir(cwd)
 
     def test_ExportVersionFromChangelogToEnvironment(self):
         cwd = TestTools.ChangeToSampleFolderAndGetCwd()
+        
+        os.environ.clear()
         VersionTools.ExportVersionFromChangelogToEnvironment('CHANGELOG.md', 'VERSION')
         self.assertEqual(os.environ['VERSION'], '1.0.0')
+        
+        os.environ.clear()
+        VersionTools.ExportVersionFromChangelogToEnvironment('CHANGELOG.v2.md', 'VERSION')
+        self.assertEqual(os.environ['VERSION'], '1.0.0')
+        
         os.chdir(cwd)
 
 
