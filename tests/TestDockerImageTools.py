d6638912481bd05ddd6bd747a9d1fd28d9dfafce
Hans Erik Heggem <heh@dips.no>
2019-10-04 14:27:31 +0200

    trying to fix test

--- tests/TestDockerImageTools.py
+++ tests/TestDockerImageTools.py
@@ -39,6 +39,7 @@ class TestDockerImageTools(unittest.TestCase):
     def test_f_CopyFromContainerToHost(self):
         print('COPY FROM CONTAINER TO HOST')
         DockerImageTools.CopyFromContainerToHost(TEST_CONTAINER_NAME, 'src/', os.path.join(TestTools.TEST_SAMPLE_FOLDER, 'output/'))
+        print(os.listdir(os.path.join(TestTools.TEST_SAMPLE_FOLDER, 'output/src')))
         self.assertTrue(os.path.isfile(os.path.join(TestTools.TEST_SAMPLE_FOLDER, 'output/src/pythonSnippet.py')))
         print('DONE COPY FROM CONTAINER TO HOST')
 
