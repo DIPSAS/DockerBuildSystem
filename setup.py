e73db88140fb91f812df44e85d8058922e57f81f
Ida Schulstad <isc@dips.no>
2019-11-18 12:31:54 +0100

    Added semantic-version as dependency

--- setup.py
+++ setup.py
@@ -133,7 +133,7 @@ setup(
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['python-dotenv', 'pyyaml'],  # Optional
+    install_requires=['python-dotenv', 'pyyaml', 'semantic-version'],  # Optional
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
